1cb72619d856d3173a2dfd8804a7880016e4b30a
zhou <972125193@qq.com>
2018-11-18 18:47:53 +0800

    突破性进展

--- read_write_xml/src/read_write_xml/Main.java
+++ read_write_xml/src/read_write_xml/Main.java
@@ -6,6 +6,7 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 import org.dom4j.Element;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,38 +17,53 @@ public class Main {
 		//String task = args[1];
 		String Ciconfig = "CIconfig.xml";
 		String task = "task.xml";
-		File Ciconfig_file = new File(Ciconfig);
-		File task_file = new File(task);
+		
+		//解析CIconfig.xml文件的pclint任务节点
+		ArrayList<String> BaseLine_task = analysis_cicloud_task(Ciconfig,task);
+		
+		//解析cicloud_task.xml文件的任务
+		
+		
+		//云构建任务与基线任务比较，增减更新任务节点
+		
+		
+	}
+	
+	public static ArrayList<String> analysis_cicloud_task(String Ciconfig,String task) {
+		//List是一个接口类，不能构造对象；而ArrayList是一个类，可以创建对象
+		ArrayList<String> BaseLine_task = new ArrayList<String>();
 		
 		try {
 			SAXReader reader = new SAXReader();
-			Document document = reader.read(Ciconfig_file);//加载xml文件，获取document对象
-			Element project_Numbers = document.getRootElement();//获取document对象根节点
-			Iterator it = project_Numbers.elementIterator();//获取迭代器
+			File task_file = new File(task);
+			Document document = reader.read(task_file);//加载xml文件，获取document对象
+			Element root_node = document.getRootElement();//获取document对象根节点
+			System.out.println("根节点名" + root_node.getName());
 			
-			//如果根节点遍历完成，则返回给while循环false
-			while (it.hasNext()) {
-				Element project = (Element) it.next();//next()方法，指针(光标)指向下一条记录，相当于遍历子节点
-				List<Attribute> project_attr_list = project.attributes();//把子节点的所有记录存储在列表
-				for (Attribute attr : project_attr_list) {
-					System.out.println("属性名: " + attr.getName());
-					System.out.println("属性值: " + attr.getValue());
+			List<Element> sub_nodes_list = root_node.elements();//获取根节点下所有子节点，并把子节点的所有记录存储在列表
+			for (Element sub_node : sub_nodes_list) {
+				System.out.println("子节点: " + sub_node.getName());
+				List<Element> pclint_nodes_list = sub_node.elements();
+				for (Element pclint_node : pclint_nodes_list) {
+					System.out.println("子节点: " + pclint_node.getName());
+					
+					
+				}
+					
+					//System.out.println("属性名: " + attr.getName());
+					//System.out.println("属性值: " + attr.getValue());
 					//这种while加for循环的解析方式，会把子节点多个属性解析出来
 					
 					
 				}
 				
-				
-				
-			}
+
 
 		} catch (DocumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
-		
+		return BaseLine_task;
 	}
 	
 }
