111c3cb479d80738a91455e7818fe670c27e1632
zhouhongyan <37979740+zhy123345@users.noreply.github.com>
2018-11-19 09:01:18 +0800

    Update temporary_file.java

--- temporary_file.java
+++ temporary_file.java
@@ -1,7 +1,1 @@
 
-<ruleSet>
-			<pclint defaultLntFile="V8R6C00/testcode/pclint/Pclint_conf/v8r6c00_frame_conf.lnt" defaultenv="v8r6c00_code=%CODE_PATH%/V8R6C00;;INCLUDE=C:/pub_code/include;pub_code=%CODE_PATH%/V8R6C00/testcode/pclint/pub_code" defaultEmakeRoot="">		
-            <!-- vrp start -->
-            <module name="pclint_vrp_opf" lntFile="V8R6C00/testcode/pclint/Pclint_conf/v8r6c00_vrp_opf.lnt" env="v8r6c00_code=%CODE_PATH%/V8R6C00;pub_code=%CODE_PATH%/V8R6C00/testcode/pclint/pub_code;EMAKE_CM=10.178.112.106; EMAKEFLAGS=--emake-impersonate-user=eauser0217 --emake-class=windows" emakeRoot="%CODE_PATH%/V8R6C00;%CODE_PATH%/V8R6C00/testcode/pclint/pub_code"/>
-
-</ruleSet>
